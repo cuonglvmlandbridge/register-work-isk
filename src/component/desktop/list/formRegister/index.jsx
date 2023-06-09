23a38213547abeade72a99076f7cb96e96dcc151
cuonglvmlandbridge <133551437+cuonglvmlandbridge@users.noreply.github.com>
2023-06-09 23:13:21 +0700

    Revert "Staging"

--- src/component/desktop/list/formRegister/index.jsx
+++ src/component/desktop/list/formRegister/index.jsx
@@ -21,8 +21,8 @@ const idApp = kintone.app.getId() || kintone.mobile.app.getId();
 
 const paramsURL = new URLSearchParams(location.search);
 
-const userISK = Cookie.get('nameUserLoginTest');
-const staffIdLogin = Cookie.get('staffIdLoginTest');
+const userISK = Cookie.get('nameUserLogin');
+const staffIdLogin = Cookie.get('staffIdLogin');
 
 export default function FormRegister({
   type,
